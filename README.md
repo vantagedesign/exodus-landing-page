bc24ebbf602a295cef11b06f6f21c324aa23a7bf
Julian <julian@vantage-design.com>
2020-02-27 22:22:13 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# exodus-landing-page
-Exodus landing page
+# [Exodus landing page](https://docs.vantage-design.com/exodus)
+
+![Product thumbnail](https://github.com/vantagedesign/assets/blob/master/exodus-landing-page/exodus-thumbnail-shadow.png)
+
+
+Exodus is a landing page with an unconventional background to catch your user's attention and draw them further into your content. Featuring sections for about us, projects and contact, you'll be capable of providing your visitors with the necessary information. Built with Bootstrap 4, you can easily extend and customize the page yourself. Powered by SASS and a powerful GulpJS workflow, you can modify Bootstrap's source code and variables to make the page truly yours. Or simply take the existing style and change the content to your needs. It's all up to you.
+
+
+## Demo & documentation
+[Exodus documentation](https://docs.vantage-design.com/exodus)
+[Exodus demo](https://docs.vantage-design.com/exodus/demo)
+
+
+
+## Installation
+
+- [Download from GitHub](https://github.com/vantagedesign/exodus-landing-page/releases)
+- Clone the repo `git clone https://github.com/vantagedesign/exodus-landing-page.git`
+
+
+## Resources
+
+- [Store page](https://vantage-design.com/products/documentation-themes/4/exodus-landing-page)
+- [Demo](https://docs.vantage-design.com/exodus/demo)
+- [Documentation](https://docs.vantage-design.com/exodus)
+- [Issues](https://github.com/vantagedesign/exodus-landing-page/issues)
+- [License](https://github.com/vantagedesign/exodus-landing-page/blob/master/LICENSE)
+
+
+## Reporting issues
+
+We use GitHub Issues as the official bug tracker for the Exodus landing page. When reporting an issue, please follow the following guidelines:
+
+1. Make sure that you are using the latest version of Exodus landing page.
+2. Provide us with reproducible steps for recreating the issue, in order for us to find the issue and fix is as soon as possible.
+3. Specify your browser and version of Exodus.
+
+
+## Licensing
+
+- Copyright 2020 [Vantage Design](https://vantage-design.com)
+- Licensed under MIT (https://github.com/vantagedesign/exodus-landing-page/blob/master/LICENSE)
+
+## Useful links
+
+- [More products](https://vantage-design.com)
+- [Free products](https://vantage-design.com/products/free-products/)
+- [Premium products](https://vantage-design.com/products/premium-products/)
+- [Design sytems](https://vantage-design.com/products/design-systems/)
+- [Page templates](https://vantage-design.com/products/page-templates/)
+- [Documentation themes](https://vantage-design.com/products/documentation-themes/)
+
+### Social media
+
+- Facebook: <https://www.facebook.com/vantagedesignofficial>
+- Instagram: <https://www.instagram.com/vantagedesignofficial>
---
+++ dist/js/main.js
@@ -0,0 +1,331 @@
+$(document).ready(function(){
+
+  // Initialize icons
+  feather.replace()
+
+  // Enable smooth scrolling
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, 'swing', function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+
+
+  // Initialize FSS
+
+  //------------------------------
+  // Mesh Properties
+  //------------------------------
+  var MESH = {
+    width: 1.1,
+    height: 1.1,
+    depth: 15,
+    segments: 19,
+    slices: 8,
+    xRange: .25,
+    yRange: .25,
+    zRange: 1,
+    ambient: "#595959",
+    diffuse: "#FFFFFF",
+    speed: 0.0005
+  };
+
+  //------------------------------
+  // Light Properties
+  //------------------------------
+  var LIGHT = {
+    count: 1,
+    xyScalar: 0.9,
+    zOffset: 150,
+    ambient: '#393939',
+    diffuse: '#212121',
+    speed: 0.001,
+    gravity: 600,
+    dampening: 0.95,
+    minLimit: 10,
+    maxLimit: null,
+    minDistance: 20,
+    maxDistance: 400,
+    autopilot: true,
+    draw: false,
+    bounds: FSS.Vector3.create(),
+    step: FSS.Vector3.create(
+      Math.randomInRange(0.2, 1.0),
+      Math.randomInRange(0.2, 1.0),
+      Math.randomInRange(0.2, 1.0)
+    )
+  };
+
+  //------------------------------
+  // Render Properties
+  //------------------------------
+  var WEBGL = 'webgl';
+  var CANVAS = 'canvas';
+  var SVG = 'svg';
+  var RENDER = {
+    renderer: CANVAS
+  };
+
+ 
+  //------------------------------
+  // Global Properties
+  //------------------------------
+  var now, start = Date.now();
+  var center = FSS.Vector3.create();
+  var attractor = FSS.Vector3.create();
+  var container = document.getElementById('fss-container');
+  var renderer, scene, mesh, geometry, material;
+  var webglRenderer, canvasRenderer, svgRenderer;
+  var gui, autopilotController;
+
+  //------------------------------
+  // Methods
+  //------------------------------
+  function initialise() {
+    createRenderer();
+    createScene();
+    createMesh();
+    createLights();
+    addEventListeners();
+    resize(container.offsetWidth, container.offsetHeight);
+    animate();
+  }
+
+  function createRenderer() {
+    webglRenderer = new FSS.WebGLRenderer();
+    canvasRenderer = new FSS.CanvasRenderer();
+    svgRenderer = new FSS.SVGRenderer();
+    setRenderer(RENDER.renderer);
+  }
+
+  function setRenderer(index) {
+    if (renderer) {
+      container.removeChild(renderer.element);
+    }
+    switch(index) {
+      case WEBGL:
+        renderer = webglRenderer;
+        break;
+      case CANVAS:
+        renderer = canvasRenderer;
+        break;
+      case SVG:
+        renderer = svgRenderer;
+        break;
+    }
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
+    container.appendChild(renderer.element);
+  }
+
+  function createScene() {
+    scene = new FSS.Scene();
+  }
+
+  function createMesh() {
+    scene.remove(mesh);
+    renderer.clear();
+    geometry = new FSS.Plane(MESH.width * renderer.width, MESH.height * renderer.height, Math.round(renderer.width / 80), Math.round(renderer.height / 160));
+    material = new FSS.Material(MESH.ambient, MESH.diffuse);
+    mesh = new FSS.Mesh(geometry, material);
+    scene.add(mesh);
+
+    // Augment vertices for animation
+    var v, vertex;
+    for (v = geometry.vertices.length - 1; v >= 0; v--) {
+      vertex = geometry.vertices[v];
+      vertex.anchor = FSS.Vector3.clone(vertex.position);
+      vertex.step = FSS.Vector3.create(
+        Math.randomInRange(0.2, 1.0),
+        Math.randomInRange(0.2, 1.0),
+        Math.randomInRange(0.2, 1.0)
+      );
+      vertex.time = Math.randomInRange(0, Math.PIM2);
+    }
+  }
+
+  function createLights() {
+    var l, light;
+    for (l = scene.lights.length - 1; l >= 0; l--) {
+      light = scene.lights[l];
+      scene.remove(light);
+    }
+    renderer.clear();
+    for (l = 0; l < LIGHT.count; l++) {
+      light = new FSS.Light(LIGHT.ambient, LIGHT.diffuse);
+      light.ambientHex = light.ambient.format();
+      light.diffuseHex = light.diffuse.format();
+      scene.add(light);
+
+      // Augment light for animation
+      light.mass = Math.randomInRange(0.5, 1);
+      light.velocity = FSS.Vector3.create();
+      light.acceleration = FSS.Vector3.create();
+      light.force = FSS.Vector3.create();
+
+      // Ring SVG Circle
+      light.ring = document.createElementNS(FSS.SVGNS, 'circle');
+      light.ring.setAttributeNS(null, 'stroke', light.ambientHex);
+      light.ring.setAttributeNS(null, 'stroke-width', '0.5');
+      light.ring.setAttributeNS(null, 'fill', 'none');
+      light.ring.setAttributeNS(null, 'r', '10');
+
+      // Core SVG Circle
+      light.core = document.createElementNS(FSS.SVGNS, 'circle');
+      light.core.setAttributeNS(null, 'fill', light.diffuseHex);
+      light.core.setAttributeNS(null, 'r', '4');
+    }
+  }
+
+  function resize(width, height) {
+    renderer.setSize(width, height);
+    FSS.Vector3.set(center, renderer.halfWidth, renderer.halfHeight);
+    createMesh();
+  }
+
+  function animate() {
+    now = Date.now() - start;
+    update();
+    render();
+    requestAnimationFrame(animate);
+  }
+
+  function update() {
+    var ox, oy, oz, l, light, v, vertex, offset = MESH.depth/2;
+
+    // Update Bounds
+    FSS.Vector3.copy(LIGHT.bounds, center);
+    FSS.Vector3.multiplyScalar(LIGHT.bounds, LIGHT.xyScalar);
+
+    // Update Attractor
+    FSS.Vector3.setZ(attractor, LIGHT.zOffset);
+
+    // Overwrite the Attractor position
+    if (LIGHT.autopilot) {
+      ox = Math.sin(LIGHT.step[0] * now * LIGHT.speed);
+      oy = Math.cos(LIGHT.step[1] * now * LIGHT.speed);
+      FSS.Vector3.set(attractor,
+        LIGHT.bounds[0]*ox,
+        LIGHT.bounds[1]*oy,
+        LIGHT.zOffset);
+    }
+
+    // Animate Lights
+    for (l = scene.lights.length - 1; l >= 0; l--) {
+      light = scene.lights[l];
+
+      // Reset the z position of the light
+      FSS.Vector3.setZ(light.position, LIGHT.zOffset);
+
+      // Calculate the force Luke!
+      var D = Math.clamp(FSS.Vector3.distanceSquared(light.position, attractor), LIGHT.minDistance, LIGHT.maxDistance);
+      var F = LIGHT.gravity * light.mass / D;
+      FSS.Vector3.subtractVectors(light.force, attractor, light.position);
+      FSS.Vector3.normalise(light.force);
+      FSS.Vector3.multiplyScalar(light.force, F);
+
+      // Update the light position
+      FSS.Vector3.set(light.acceleration);
+      FSS.Vector3.add(light.acceleration, light.force);
+      FSS.Vector3.add(light.velocity, light.acceleration);
+      FSS.Vector3.multiplyScalar(light.velocity, LIGHT.dampening);
+      FSS.Vector3.limit(light.velocity, LIGHT.minLimit, LIGHT.maxLimit);
+      FSS.Vector3.add(light.position, light.velocity);
+    }
+
+    // Animate Vertices
+    for (v = geometry.vertices.length - 1; v >= 0; v--) {
+      vertex = geometry.vertices[v];
+      ox = Math.sin(vertex.time + vertex.step[0] * now * MESH.speed);
+      oy = Math.cos(vertex.time + vertex.step[1] * now * MESH.speed);
+      oz = Math.sin(vertex.time + vertex.step[2] * now * MESH.speed);
+      FSS.Vector3.set(vertex.position,
+        MESH.xRange*geometry.segmentWidth*ox,
+        MESH.yRange*geometry.sliceHeight*oy,
+        MESH.zRange*offset*oz - offset);
+      FSS.Vector3.add(vertex.position, vertex.anchor);
+    }
+
+    // Set the Geometry to dirty
+    geometry.dirty = true;
+  }
+
+  function render() {
+    renderer.render(scene);
+
+    // Draw Lights
+    if (LIGHT.draw) {
+      var l, lx, ly, light;
+      for (l = scene.lights.length - 1; l >= 0; l--) {
+        light = scene.lights[l];
+        lx = light.position[0];
+        ly = light.position[1];
+        switch(RENDER.renderer) {
+          case CANVAS:
+            renderer.context.lineWidth = 0.5;
+            renderer.context.beginPath();
+            renderer.context.arc(lx, ly, 10, 0, Math.PIM2);
+            renderer.context.strokeStyle = light.ambientHex;
+            renderer.context.stroke();
+            renderer.context.beginPath();
+            renderer.context.arc(lx, ly, 4, 0, Math.PIM2);
+            renderer.context.fillStyle = light.diffuseHex;
+            renderer.context.fill();
+            break;
+          case SVG:
+            lx += renderer.halfWidth;
+            ly = renderer.halfHeight - ly;
+            light.core.setAttributeNS(null, 'fill', light.diffuseHex);
+            light.core.setAttributeNS(null, 'cx', lx);
+            light.core.setAttributeNS(null, 'cy', ly);
+            renderer.element.appendChild(light.core);
+            light.ring.setAttributeNS(null, 'stroke', light.ambientHex);
+            light.ring.setAttributeNS(null, 'cx', lx);
+            light.ring.setAttributeNS(null, 'cy', ly);
+            renderer.element.appendChild(light.ring);
+            break;
+        }
+      }
+    }
+  }
+
+  function addEventListeners() {
+    window.addEventListener('resize', onWindowResize);
+    container.addEventListener('click', onMouseClick);
+    container.addEventListener('mousemove', onMouseMove);
+  }
+
+  //------------------------------
+  // Callbacks
+  //------------------------------
+  function onMouseClick(event) {
+    FSS.Vector3.set(attractor, event.x, renderer.height - event.y);
+    FSS.Vector3.subtract(attractor, center);
+    LIGHT.autopilot = !LIGHT.autopilot;
+    autopilotController.updateDisplay();
+  }
+
+  function onMouseMove(event) {
+    FSS.Vector3.set(attractor, event.x, renderer.height - event.y);
+    FSS.Vector3.subtract(attractor, center);
+  }
+
+  function onWindowResize(event) {
+    resize(container.offsetWidth, container.offsetHeight);
+    render();
+  }
+
+
+
+  // Let there be light!
+  initialise();
+
+
+});
