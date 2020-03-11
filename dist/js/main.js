cfbd32b02584c43342f844b91108b01c92c64ab4
Julian <julian@vantage-design.com>
2020-03-11 10:22:24 +0100

    Fixed JS

--- dist/js/main.js
+++ dist/js/main.js
@@ -25,11 +25,11 @@ $(document).ready(function(){
 
 
   // Viewport height fix
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-  window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  var vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', "".concat(vh, "px"));
+  window.addEventListener('resize', function () {
+    var vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', "".concat(vh, "px"));
   });
 
   // Initialize FSS
